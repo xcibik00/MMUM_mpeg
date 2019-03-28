8aeb87c361034ffcec06cc4b2d5e3899697b84f1
Peter Cibik <xcibik00@vutbr.cz>
2019-03-28 17:21:47 +0000

    Update Functions.java

--- MMUM_eclipse_project/src/mmum/Functions.java
+++ MMUM_eclipse_project/src/mmum/Functions.java
@@ -129,17 +129,115 @@ public static Matrix upsize_matrix (int size, Matrix input_matrix) {
 		int height = input_matrix_a.getRowDimension();
 		int width = input_matrix_a.getColumnDimension();
 		
+		System.out.println("DPCM:");
+		
 		Matrix tmp_matrix = new Matrix(height, width);
 		
 		double tmp_value = 0;
 		
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				tmp_value = (((input_matrix_a.get(i, j) + 255) - input_matrix_b.get(i, j)) / 2);
+				tmp_value = (((input_matrix_b.get(i, j) + 255) - input_matrix_a.get(i, j)) / 2);
 				tmp_matrix.set(i, j, tmp_value); 
+				System.out.print(tmp_value + " ");
+				
 			}
+			System.out.println();
 		}
 		return tmp_matrix;
 	}
+	
+public static LinkedList<int[]> FULL_search (Matrix input_matrix_a, Matrix input_matrix_b, int macroblok_size) {
+		
+		int height = input_matrix_a.getRowDimension();
+		int width = input_matrix_a.getColumnDimension();
+		
+		int start_height;
+		int start_width;
+		
+		LinkedList<int[]> tmp_list = new LinkedList<int[]>();
+		
+		Matrix upsized_matrix_a = upsize_matrix((macroblok_size / 2), input_matrix_a);
+		
+		Matrix macroblok;
+		
+		double tmp_SAD;
+		double tmp_SAD_block = 0;
+		
+		int height_v = 0;
+		int width_v = 0;
+		
+		for (int i = 0; i < (height / macroblok_size); i++) {
+			
+			for (int j = 0; j < (width / macroblok_size); j++) {
+				
+				tmp_SAD = 100000.0;
+				
+				macroblok = input_matrix_b.getMatrix((i * macroblok_size), (((i + 1) * macroblok_size) - 1), (j * macroblok_size), (((j + 1) * macroblok_size) - 1));
+				
+				start_height = i * macroblok_size;
+				start_width = j * macroblok_size;
+				
+				for (int m = 0; m <= macroblok_size; m++) {
+					for (int n = 0; n <= macroblok_size; n++) {
+						
+						tmp_SAD_block = SAD(macroblok, upsized_matrix_a.getMatrix((start_height + m), ((start_height + m + macroblok_size) - 1), (start_width + n), ((start_width + n + macroblok_size) - 1) ));
+						
+						if(tmp_SAD > tmp_SAD_block) {
+							tmp_SAD = tmp_SAD_block;
+							height_v = start_height + m;
+							width_v = start_width + n;
+						}
+						
+					}
+				}
+				
+				System.out.print("( " + height_v + " , " + width_v + " ) ; ");
+				tmp_list.add(new int[] {height_v, width_v});
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+		return tmp_list;
+	}
+
+	public static Matrix FULL_search_i(LinkedList<int[]> vectors, Matrix input_matrix_b, int macroblok_size) {
+		
+		int height = input_matrix_b.getRowDimension();
+		int width = input_matrix_b.getColumnDimension();
+		
+		int height_v;
+		int width_v;
+		
+		int[] tmp_pole;
+		
+		Matrix tmp_matrix = new Matrix(height, width);
+		Matrix upsized_matrix_b;
+		
+		for (int i = 0; i < (height / macroblok_size); i++) {
+			
+			for (int j = 0; j < (width / macroblok_size); j++) {
+				
+				Matrix macroblok;
+				
+				tmp_pole = vectors.poll();
+				height_v = tmp_pole[0];
+				width_v	= tmp_pole[1];
+				
+				upsized_matrix_b = upsize_matrix((macroblok_size / 2), input_matrix_b);
+				
+				macroblok = upsized_matrix_b.getMatrix(height_v, ((height_v + macroblok_size) - 1), width_v, ((width_v + macroblok_size) - 1));
+				
+				tmp_matrix.setMatrix((i * macroblok_size), (((i + 1) * macroblok_size) - 1), (j * macroblok_size), (((j + 1) * macroblok_size) - 1), macroblok);
+			}
+		}
+		
+		
+		return tmp_matrix;
+		
+	} 
 	
 }
